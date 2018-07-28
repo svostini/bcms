ee4d1b7da686e214b054417702189a455a863640
Snezana Vostinic <snezana.vostinic@cern.ch>
2018-07-28 22:01:08 +0200

    files

---
+++ js/topnav.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    'use strict';
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+      
+    } else {
+        x.className = "topnav";
+       
+    }
+}
